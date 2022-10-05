b9635cd09ad39091932d9f70d022eb4475911303
kimitawanjohi <kimitawanjohi7923@gmail.com>
2022-10-05 20:27:17 +0300

    done setting up routes and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Box from '@mui/material/Box';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Feed, Navbar, VideoDetail, ChannelDetail, SearchFeed } from './components'
 
 function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   )
 }
 
