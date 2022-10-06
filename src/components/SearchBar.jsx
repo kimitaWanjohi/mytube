006d677e9f996ba288b7a65e7e3adba0b7e8c572
kimitawanjohi <kimitawanjohi7923@gmail.com>
2022-10-06 10:49:51 +0300

    search page and functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,10 +6,21 @@ import {Search} from '@mui/icons-material';
 
 
 function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      if(searchTerm) {
+          navigate(`/search/${searchTerm}`);
+          setSearchTerm('');
+      }
+  }
+    
   return (
     <Paper
         component="form"
-        onSubmit={() => {}}
+        onSubmit={handleSubmit}
         sx={{
             borderRadius: 20,
             border: '1px solid #e3e3e3',
@@ -21,8 +32,8 @@ function SearchBar() {
         <input
             className="search-bar"
             placeholder="Search Video..."
-            value=""
-            onChange={()=>{}}
+            value={searchTerm}
+            onChange={(e)=>setSearchTerm(e.target.value)}
         />
         <IconButton 
             type="submit"
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import {useParams} from 'react-router-dom';
+import Videos from './Videos';
 
-function SearchFeed() {
+import {fetchFromAPI} from '../utils/fetchFromApi';
+
+export default function SearchFeed() {
+  const [videos, setVideos] = useState();
+  const {searchTerm} = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+  }, [searchTerm])
+
   return (
-    <div>SearchFeed</div>
+    <Box
+      p={2}
+      sx={{
+        overflowY: 'auto',
+        height: '90vh',
+        flex: 2
+      }}
+    >
+      <Typography
+        variant="h4"
+        component="div"
+        fontWeight="bold"
+        gutterBottom
+        sx={{
+          color: "white",
+          marginLeft: '1px'
+        }}
+      >
+        Search Results for: 
+        <span
+          style={{
+            color: "#F31503"
+          }}
+        >
+          {' '} {searchTerm} {' '}
+        </span>Videos
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
   )
 }
 
-export default SearchFeed
+
