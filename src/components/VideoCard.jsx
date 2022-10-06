ec7ca19b4b5fedac599e5dd48f0de1c8b882974e
kimitawanjohi <kimitawanjohi7923@gmail.com>
2022-10-06 08:41:49 +0300

    done with the video card

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -17,13 +17,46 @@ import {
 
 function VideoCard({ video: {id: {videoId}, snippet}}) {
   return (
-    <Card>
+    <Card
+        sx={{
+            width: { md: "320px", xs: "100%"},
+            boxShadow: 'none', borderRadius: 0
+        }}
+    >
         <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
             <CardMedia 
                 image={snippet?.thumbnails?.high?.url}
                 alt={snippet?.title}
                 sx={{width: 358, height: 180}}
             />
+            <CardContent sx={{
+                backgroundColor: '#1e1e1e',
+                height: "106px"
+            }}>
+                <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+                    <Typography
+                        variant="subtitle1"
+                        fontWeight="bold"
+                        color="#FFF"
+                    >
+                        {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+                    </Typography>
+                </Link>
+                <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+                    <Typography
+                        variant="subtitle2"
+                        fontWeight="bold"
+                        color="gray"
+                    >
+                        {snippet?.channelTitle || demoChannelTitle }
+                        <CheckCircle sx={{
+                            fontSize: 12,
+                            color: 'gray',
+                            ml: 5
+                        }}/>
+                    </Typography>
+                </Link>
+            </CardContent>
         </Link>
     </Card>
   )
