257111fb48f1018295e4cb4b796ec159b1ac761f
kimitawanjohi <kimitawanjohi7923@gmail.com>
2022-10-09 09:54:26 +0300

    fixed the searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,59 @@
 import React, {useState} from 'react';
+import { styled, alpha } from '@mui/material/styles';
 import {useNavigate} from 'react-router-dom';
-import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import {Search} from '@mui/icons-material';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
 
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    height: '40px',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    height: '40px',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+      [theme.breakpoints.down('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
 
 function SearchBar() {
   const [searchTerm, setSearchTerm] = useState();
@@ -18,35 +68,19 @@ function SearchBar() {
   }
     
   return (
-    <Paper
-        component="form"
-        onSubmit={handleSubmit}
-        sx={{
-            display: 'flex',
-            alignItems: 'center',
-            borderRadius: 20,
-            border: '1px solid #e3e3e3',
-            pl: 2,
-            boxShadow: 'none',
-            mr: {sm: 5}
-        }}
-    >
-        <input
-            className="search-bar"
-            placeholder="Search Video..."
+    <form onSubmit={handleSubmit}> 
+        <Search>
+            <SearchIconWrapper>
+                <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+            placeholder="Search…"
             value={searchTerm}
-            onChange={(e)=>setSearchTerm(e.target.value)}
-        />
-        <IconButton 
-            type="submit"
-            sx={{
-                p: "10px",
-                color: "red" 
-            }}
-        >
-            <Search />
-        </IconButton>
-    </Paper>
+            onChange={(e) => setSearchTerm(e.target.value)}
+            inputProps={{ 'aria-label': 'search' }}
+            />
+        </Search>
+    </form>
   )
 }
 
